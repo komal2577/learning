735fd5828b9720f43a04ba42ec95d9d8d5aa1c17
komal <pawarkomal225@gmail.com>
2022-04-11 14:20:29 +0530

    junit

--- src/test/java/com/junitguide/springboottesting/controller/EmployeeControllerTest.java
+++ src/test/java/com/junitguide/springboottesting/controller/EmployeeControllerTest.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Optional;
 
 @WebMvcTest
-public class EmployeeControllerTest {
+class EmployeeControllerTest {
 	
     @Autowired
     private MockMvc mockMvc;
